a00699ed5bf236fc191df9bc26a030492629f593
Divadsjo <70754118+Divadsjo@users.noreply.github.com>
2021-03-26 12:15:17 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,24 +1,6 @@
-def fakultet(n):
-    sum = 1
-    for x in range(1,n+1):
-        sum *= x
-    return sum
+import tkinter as tk
+tk._test()
 
-# print(fakultet(int(input("n = "))))
-
-# (b) och beräknar följande summa s = 1 + 1 +
-
-def inverteradFakultet(n):
-    return sum([1/fakultet(i) for i in range(n+1)])
-
-# print(inverteradFakultet(int(input("n = "))))
-
-
-# (**)
-
-# (c) och beräknar följande summa s = 1 −
-
-def inverteradOjämnt(n):
-    return sum([((-1)**(i+1))/(2*i-1) for i in range(1,n+1)])
-while True:
-    print(inverteradOjämnt(int(input("n = "))))
+greeting = tk.Label(text="Hello world")
+greeting.pack
+window = tk.Tk()
